9ea50b0229b1d6a10da3b0f7aaec27f24bc4f34c
musi96 <museabmohammed23@gmail.com>
2025-07-07 13:15:47 +0300

    news_scraper.py

--- news_scraper.py
+++ news_scraper.py
@@ -1,6 +1,6 @@
 def get_econ_news():
     return [
-        "📰 IMF revises global growth forecast upward for 2025.",
-        "📰 Ethiopia signs new trade deal with East African nations.",
-        "📰 Inflation in Sub-Saharan Africa expected to drop in Q3."
+        "IMF revises global growth forecast upward for 2025.",
+        "Ethiopia signs new trade deal with East African nations.",
+        "Inflation in Sub-Saharan Africa expected to drop in Q3."
     ]

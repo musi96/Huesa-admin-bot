db7e2f2f4b3a59e7b7bba6e97ca53250e4641bdb
musi96 <museabmohammed23@gmail.com>
2025-07-07 13:17:02 +0300

    scholarships_scraper.py

--- scholarships_scraper.py
+++ scholarships_scraper.py
@@ -1,6 +1,6 @@
 def get_scholarships():
     return [
-        "🎓 DAAD Scholarship 2025 for MSc/PhD - Open to Ethiopians.\nApply: https://daad.de",
-        "🎓 Erasmus Mundus 2025 is open – full funding for Economics.\nApply: https://erasmus-plus.ec.europa.eu",
-        "🎓 African Union scholarships now available for graduate students.\nApply: https://au.int"
+        "DAAD Scholarship 2025 for MSc/PhD - Open to Ethiopians.\nApply: https://daad.de",
+        "Erasmus Mundus 2025 is open – full funding for Economics.\nApply: https://erasmus-plus.ec.europa.eu",
+        "African Union scholarships now available for graduate students.\nApply: https://au.int"
     ]
